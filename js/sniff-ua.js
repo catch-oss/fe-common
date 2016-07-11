af27bf9f67b55400ca8ef307f25947db0914280d
David White <david.white@catch.co.nz>
2016-07-12 10:01:16 +1200

    Add ie10 and ie11 sniff

--- js/sniff-ua.js
+++ js/sniff-ua.js
@@ -44,6 +44,37 @@
 					}
 				}
 			}
+
+            // Conditional HTML comments don't work for ie10+ so UA sniff it ...
+            var getInternetExplorerVersion = function() {
+
+                var rv = -1;
+
+                if (navigator.appName == 'Microsoft Internet Explorer'){
+                    var ua = navigator.userAgent;
+                    var re  = new RegExp('MSIE ([0-9]{1,}[\.0-9]{0,})');
+
+                    if (re.exec(ua) !== null) {
+                        rv = parseFloat( RegExp.$1 );
+                    }
+                }
+
+                else if (navigator.appName == 'Netscape'){
+                    var ua = navigator.userAgent;
+                    var re  = new RegExp('Trident/.*rv:([0-9]{1,}[\.0-9]{0,})');
+
+                    if (re.exec(ua) !== null) {
+                        rv = parseFloat( RegExp.$1 );
+                    }
+                }
+
+                return rv;
+            };
+
+            // let everyone know it's ie10 or greater
+            if (getInternetExplorerVersion() >= 10){
+                $('html').addClass('ie gt-ie9');
+            }
 		});
 	};
 }));
