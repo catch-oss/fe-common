5466ddec9e8ac9110fe0b13e8a02ad18dbaa5394
Az <breaks.nz@gmail.com>
2016-12-12 09:54:09 +1300

    added utils

---
+++ js/util.js
@@ -0,0 +1,30 @@
+;(function (root, factory) {
+
+    // AMD. Register as an anonymous module depending on jQuery.
+    if (typeof define === 'function' && define.amd) define([], factory);
+
+    // Node, CommonJS-like
+    else if (typeof exports === 'object') module.exports = factory();
+
+    // Browser globals (root is window)
+    else {
+        root.catch = (root.catch || {});
+        root.catch.util = factory();
+    }
+
+}(this, function ($, popstate, undefined) {
+
+    'use strict';
+
+    return {
+        uuid: function() {
+            let d = new Date().getTime(),
+                uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                    let r = (d + Math.random() * 16) % 16 | 0;
+                    d = Math.floor(d / 16);
+                    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+                });
+            return uuid;
+        }
+    }
+}))
