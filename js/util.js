0a62bfa72dce30edd837f50c3329126b77ba0a98
Az <breaks.nz@gmail.com>
2016-12-12 13:37:08 +1300

    classic es6

--- js/util.js
+++ js/util.js
@@ -18,9 +18,9 @@
 
     return {
         uuid: function() {
-            let d = new Date().getTime(),
+            var d = new Date().getTime(),
                 uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                    let r = (d + Math.random() * 16) % 16 | 0;
+                    var r = (d + Math.random() * 16) % 16 | 0;
                     d = Math.floor(d / 16);
                     return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
                 });
