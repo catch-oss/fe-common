9357dddec62e735c4defe101b9cd8aca85bbefc1
Az Ilari <breaks.nz@gmail.com>
2017-09-15 11:28:04 +1200

    commit for release 3.1.0

---
+++ js/last-hash.js
@@ -0,0 +1,42 @@
+;(function (root, factory) {
+
+    // AMD. Register as an anonymous module depending on jQuery.
+    if (typeof define === 'function' && define.amd)
+        define(
+            [
+                'jquery'
+            ],
+            factory
+        );
+
+    // Node, CommonJS-like
+    else if (typeof exports === 'object')
+        module.exports = factory(
+            require('jquery')
+        );
+
+    // Browser globals (root is window)
+    else {
+        root.catch = (root.catch || {});
+        root.catch.lastHash = factory(
+            root.jQuery
+        );
+    }
+
+}(this, function ($, undefined) {
+
+    'use strict';
+
+    return function(opts) {
+
+        opts = opts || {};
+        opts.selector = 'a[href*="#"]';
+
+        $(opts.selector)
+            .off('hashchange.lastHash')
+            .off('click.lastHash')
+            .on('click.lastHash hashchange.lastHash', function(e) {
+                document.cookie = 'lastHash=' + this.href.split('#')[1];
+            });
+    }
+}));
