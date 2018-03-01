5aa710ba826638d6826ac29429375ed458d0eb4e
Az Ilari <breaks.nz@gmail.com>
2018-03-01 15:22:08 +1300

    fixed a browser global and added a new script

--- js/conditional-props.js
+++ js/conditional-props.js
@@ -22,7 +22,7 @@
      // Browser globals (root is window)
      else {
          root.catch = (root.catch || {});
-         root.catch.conditionalClasses = factory(
+         root.catch.conditionalProps = factory(
              root.jQuery,
              root.catch.util
          );
@@ -30,36 +30,38 @@
 
 }(this, function ($, util) {
 
-    var handle = function($el, condition, classes) {
-        if (util.testCondition(condition)) $el.addClass(classes);
-        else $el.removeClass(classes);
+    var handle = function($el, condition, prop, trueVal, falseVal) {
+        if (util.testCondition(condition)) $el.prop(prop, trueVal);
+        else $el.prop(prop, falseVal);
     };
 
     return function() {
 
         $(function() {
 
-            $('[data-condition][data-class]').each(function() {
+            $('[data-prop-condition][data-prop]').each(function() {
 
                 // extract the config
                 var $el = $(this),
                     elID = util.elemId($el),
-                    condition = $el.attr('data-condition'),
-                    classes = $el.attr('data-class'),
-                    bindSelector = $el.attr('data-bind-selector'),
-                    bindEvent = $el.attr('data-bind-event'),
-                    eName = bindEvent + '.condclass' + elID,
-                    eNameDummy = 'dummy' + '.condclass' + elID;
+                    condition = $el.attr('data-prop-condition'),
+                    prop = $el.attr('data-prop'),
+                    trueVal = $el.attr('data-prop-true-val'),
+                    falseVal = $el.attr('data-prop-false-val'),
+                    bindSelector = $el.attr('data-prop-bind-selector'),
+                    bindEvent = $el.attr('data-prop-bind-event'),
+                    eName = bindEvent + '.condprop' + elID,
+                    eNameDummy = 'dummy' + '.condprop' + elID;
 
                 // do the comparison
-                handle($el, condition, classes);
+                handle($el, condition, prop, trueVal, falseVal);
 
                 // bind event handlers
                 $(bindSelector)
                     .off(eName)
                     .off(eNameDummy)
                     .on(eName + ' ' + eNameDummy, function() {
-                        handle($el, condition, classes);
+                        handle($el, condition, prop, trueVal, falseVal);
                     });
             });
         });
